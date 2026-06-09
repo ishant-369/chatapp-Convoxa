ca1a0f359846e19f675068fbc76009d14e1b0781
ishant-369 <isntae@gmail.com>
2026-06-09 21:06:55 +0530

    Initial frontend project

---
+++ randim.js
@@ -0,0 +1,13 @@
+const usersContainer = document.getElementById('users-container');
+function addUser(username){
+    const userDiv = document.createElement("div");
+    userDiv.classList.add("friend");
+    userDiv.textContent = username;
+    usersContainer.appendChild(userDiv);    
+    window.alert("A girl from your area wants to sleep with you");
+    //for temporarily testing cause i dont have backend rn so i will just user add user type thing instead of the fetch function
+    addUser("joe goldberg");
+    addUser("tony stark");
+    addUser("thomas shelby");
+
+}
