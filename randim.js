fccf52df63defa390eae64857e30aa07cce6ab52
ishant-369 <isntae@gmail.com>
2026-06-22 16:34:43 +0530

    new update

--- randim.js
+++ randim.js
@@ -4,10 +4,14 @@ function addUser(username){
     userDiv.classList.add("friend");
     userDiv.textContent = username;
     usersContainer.appendChild(userDiv);    
+    
     window.alert("A girl from your area wants to sleep with you");
-    //for temporarily testing cause i dont have backend rn so i will just user add user type thing instead of the fetch function
-    addUser("joe goldberg");
-    addUser("tony stark");
-    addUser("thomas shelby");
+    
+    
 
 }
+//for temporarily testing cause i dont have backend rn so i will just user add user type thing instead of the fetch function
+    //addUser("joe goldberg");
+    //addUser("tony stark");
+    //addUser("thomas shelby");
+    
